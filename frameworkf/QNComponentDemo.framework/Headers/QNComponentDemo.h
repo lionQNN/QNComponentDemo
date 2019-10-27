bf14959e69ebf0581eeeca247d979df6a1bdfec2
qinnn <qinningn@163.com>
2019-10-28 00:26:39 +0800

    这是静态库

---
+++ frameworkf/QNComponentDemo.framework/Headers/QNComponentDemo.h
@@ -0,0 +1,19 @@
+//
+//  QNComponentDemo.h
+//  QNComponentDemo
+//
+//  Created by QNN on 2019/10/28.
+//  Copyright © 2019 QNN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QNComponentDemo.
+FOUNDATION_EXPORT double QNComponentDemoVersionNumber;
+
+//! Project version string for QNComponentDemo.
+FOUNDATION_EXPORT const unsigned char QNComponentDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QNComponentDemo/PublicHeader.h>
+
+
---
+++ frameworkf/QNComponentDemo.framework/Headers/QNPrintUtil.h
@@ -0,0 +1,21 @@
+//
+//  QNPrintUtil.h
+//  QNComponentDemo
+//
+//  Created by QNN on 2019/10/28.
+//  Copyright © 2019 QNN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// 打印工具类
+@interface QNPrintUtil : NSObject
+
+
+/// 打印信息
++(void)printInfo;
+@end
+
+NS_ASSUME_NONNULL_END
